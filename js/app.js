5deddf2af9b8346b2a88edf7604c61d44b9a24de
timiodulate <timiodulate@gmail.com>
2020-05-12 14:12:02 +0100

    added responsiveness

--- js/app.js
+++ js/app.js
@@ -1,12 +1,15 @@
 var checkbox = document.querySelector('input[name=theme]');
+var mode = document.getElementById('modeText')
 
 checkbox.addEventListener('change', function() {
     if(this.checked) {
         trans()
         document.documentElement.setAttribute('data-theme', 'dark')
+        // mode.textContent = 'Dark Mode'
     } else {
         trans()
         document.documentElement.setAttribute('data-theme', 'light')
+        // mode.textContent ='Light Mode'
     }
 })
 
